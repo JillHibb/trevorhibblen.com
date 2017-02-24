aba7f8f29e817ba330f1254cfb245243b5dee762
Trevor Hibblen <hibblen.trevor@gmail.com>
2017-02-23 19:33:59 -0600

    skillz again

--- pages/skills/skills.controller.js
+++ pages/skills/skills.controller.js
@@ -12,9 +12,9 @@
     _vm.skills = [
       'Languages: C#, Javascript, T-SQL',
       'Server-side: NodeJs, ExpressJs, .Net MVC/WebApi, .Net Webforms',
-      'Client-side: jQuery, AngularJs, ReactJs',
-      'Database: MSSQL, MySQL, MongoDb, Redis, RavenDb / Mongoose, NHibernate, PetaPoco',
-      'Tools: AWS, Docker Compose, RabbitMQ, IIS, Bash, Git, Less, SASS, Gulp, Webpack, Visual Studio',
+      'Client-side: jQuery, Angular 1, ReactJs, SSE, WebSockets',
+      'Database: MSSQL, MySQL, MongoDb, Redis, RavenDb / Mongoose, NHibernate, PetaPoco, GraphQL',
+      'Tools: Bash, Git, AWS, Docker Compose, RabbitMQ, IIS, Gulp, Webpack, Visual Studio, NUnit, Mocha, Jasmine',
       'Concepts: SOLID, DDD, DRY, REST, SPA, OWASP, 12 Factor App'
     ];
   }
